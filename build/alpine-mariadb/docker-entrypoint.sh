215b0e7eb35c547696ce854d682df0d65cae0b09
Mickaël Wolff <m@lupusmic.org>
2019-05-31 00:16:08 +0200

    Alpine MariaDB: fix user creation

--- build/alpine-mariadb/docker-entrypoint.sh
+++ build/alpine-mariadb/docker-entrypoint.sh
@@ -3,20 +3,21 @@
 sql_tmp=$(mktemp)
 
 cat << SQL > $sql_tmp
-create user '${MYSQL_USER_NAME}'@'%' identified by '${MYSQL_USER_PASSWORD}';
-create user '${MYSQL_USER_NAME}'@'localhost' identified by '${MYSQL_USER_PASSWORD}';
 create database ${MYSQL_USER_DATABASE};
+create user '${MYSQL_USER_NAME}'@'%' identified by '${MYSQL_USER_PASSWORD}';
 grant all privileges on ${MYSQL_USER_DATABASE}.* to '${MYSQL_USER_NAME}'@'%';
-grant all privileges on ${MYSQL_USER_DATABASE}.* to '${MYSQL_USER_NAME}'@'localhost';
 flush privileges;
 SQL
 
-mysqld &
+mysqld \
+    --datadir "${MYSQL_DATA_PATH}" \
+    &
+
 pid="$!"
 
 sleep 2 # Wait for daemon to start up
 
-cat $sql_tmp | mysql -uroot
+cat $sql_tmp | mysql -uroot mysql
 
 kill -s TERM $pid
 wait $pid
