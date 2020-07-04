1e5c8c674fbe73a1e1ec0f8a354e966fb054a03b
Mickaël Wolff <mickael@lupusmic.org>
2020-07-04 10:59:18 +0200

    Use a base image igor to embed management scripts

--- build/igor/docker-entrypoint.sh
+++ build/igor/docker-entrypoint.sh
@@ -7,19 +7,41 @@ set -e
 [[ ! -z "$DEBUG" ]] \
 	&& set -x
 
+lp-distribution-get()
+{
+	. /etc/os-release
+	echo "$ID"
+}
+
 lp-init()
 {
 	lp-assert-environement-is-set ANVIL
 	lp-assert-environement-is-set UID
 	lp-assert-environement-is-set USER_ALIAS
 
-	id "${USER_ALIAS}" 2>&1 1>&/dev/null \
-		|| adduser \
-			--uid "${UID}" \
-			--gecos '""' \
-			--disabled-password \
-			--home /home \
-			"${USER_ALIAS}"
+	case "$(lp-distribution-get)" in
+		debian)
+			id "${USER_ALIAS}" 2>&1 1>&/dev/null \
+				|| adduser \
+					--uid "${UID}" \
+					--gecos '""' \
+					--disabled-password \
+					--home /home \
+					"${USER_ALIAS}"
+		;;
+		alpine)
+			id "${USER_ALIAS}" 2>&1 1>&/dev/null \
+				|| adduser \
+					-u "$UID" \
+					-g "" \
+					-D \
+					-h /home \
+					"${USER_ALIAS}"
+		;;
+		*)
+			lp-die "Unsupported distribution '$OS'"
+		;;
+	esac
 
 	mkdir -p "${ANVIL}"
 	cd "${ANVIL}"
