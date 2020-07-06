851f8ad5a1b0c64e00285a8df019db58541b05d4
Mickaël Wolff <mickael@lupusmic.org>
2020-07-07 01:37:44 +0200

    NodeJS images build  from entrypoint

--- build/alpine-nodejs/docker-entrypoint.sh
+++ build/alpine-nodejs/docker-entrypoint.sh
@@ -2,14 +2,17 @@
 
 if [ ! -f package.json ]
 then
-    cp \
-		"${LUPUSMICHAELIS_DIR}/node.package.json-dist" \
-		package.json
+	gosu "${USER_ALIAS}" \
+		cp \
+			"${LUPUSMICHAELIS_DIR}/node.package.json-dist" \
+			package.json
 
-	npm i --save \
-		json-server@0.9.5
-
-	chown -R $UID .
+	gosu "${USER_ALIAS}" \
+		npm i --save \
+			json-server@0.9.5
+else
+	gosu "${USER_ALIAS}" \
+		npm i
 fi
 
 exec "$@"
