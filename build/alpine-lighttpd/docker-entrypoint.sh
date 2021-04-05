2aff7ac290d3007950b700531029fc6995a34b42
Mickaël Wolff <mickael@lupusmic.org>
2021-04-05 02:03:11 +0200

    Alpine secured lighttpd build in same context as vanilla lighttpd

---
+++ build/alpine-lighttpd/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. "$LUPUSMICHAELIS_DIR/lp-lighttpd.sh"
+
+lp-lighttpd-generate-tls
---
+++ build/alpine-lighttpd/library.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+lp-lighttpd-generate-tls()
+{
+	openssl \
+		req \
+		-subj '/CN=lupusmic.org/O=None/C=FR' \
+		-newkey rsa:2048 \
+		-nodes \
+		-keyout /etc/lighttpd/server.key \
+		-x509 \
+		-days 365 \
+		-out /etc/lighttpd/server.crt
+
+	cat \
+		/etc/lighttpd/server.key \
+		/etc/lighttpd/server.crt \
+		> /etc/lighttpd/server.pem
+
+	chmod 400 \
+		/etc/lighttpd/server.key \
+		/etc/lighttpd/server.crt \
+		/etc/lighttpd/server.pem
+
+	chown lighttpd:lighttpd \
+		/etc/lighttpd/server.key \
+		/etc/lighttpd/server.crt \
+		/etc/lighttpd/server.pem
+}
