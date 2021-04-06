4b764a829dc7d8c58d9c08ade2a6f7c88eeafe46
Mickaël Wolff <m@lupusmic.org>
2021-04-06 08:23:01 +0000

    Alpine lighttpd: consider rewriting on entry point

--- build/alpine-lighttpd/docker-entrypoint.sh
+++ build/alpine-lighttpd/docker-entrypoint.sh
@@ -19,6 +19,18 @@ main()
 		lp-assert-environement-is-set BACKEND
 		lp-assert-environement-is-set BACKEND_DOCROOT
 	fi
+
+	if [[ "disable" != "$LIGHTTPD_REWRITE" && "/index.php" != "$LIGHTTPD_TARGET" ]]
+	then
+		case "${LIGHTTPD_REWRITE}" in
+        always)
+            echo 'url.rewrite-once = ( "^.*$" => "'"$LIGHTTPD_TARGET"'" )'
+            ;;
+        when-not-found)
+            echo 'url.rewrite-if-not-file = ( "^/.*$" => "'"$LIGHTTPD_TARGET"'" )'
+            ;;
+        esac > /etc/lighttpd/mod_rewrite.conf
+	fi
 }
 
 main "$@"
