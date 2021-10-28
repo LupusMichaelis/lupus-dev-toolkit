e1e98ff018a7e6bb3e99de1af4e48cb451a66db3
Mickaël Wolff <m@lupusmic.org>
2021-10-28 12:21:52 +0200

    Alpine MariaDB: reorganize schema first

--- build/alpine-mariadb/docker-entrypoint.sh
+++ build/alpine-mariadb/docker-entrypoint.sh
@@ -27,11 +27,26 @@ function init()
 	sleep 2 # Wait for daemon to start up
 
 	{
-		if [ -n "$MYSQL_ROOT_PASSWORD" ]
+		if [ -d $INIT_DIR/schema ]
 		then
-			echo "create user 'root'@'%' identified by '{$MYSQL_ROOT_PASSWORD}';"
-			echo "create user '${MYSQL_SYSUSER}'@'%' identified by '${MYSQL_ROOT_PASSWORD}';"
+			for schema in $(ls $INIT_DIR/schema/*.sql)
+			do
+				space=$(basename $schema .sql) # XXX Test not busybox
+				echo "-- $schema --"
+				echo "create database if not exists \`$space\`;"
+				echo "use \`$space\`;"
+				cat $schema
+			done
+            echo
+		fi
+
+		if [[ -v MYSQL_ROOT_PASSWORD && -n "$MYSQL_ROOT_PASSWORD" ]]
+		then
 			echo "create user '${LP_DEV_USER_ALIAS}'@'%' identified by '${MYSQL_ROOT_PASSWORD}';"
+			cat <<- SQL
+			create user if not exists '${LP_DEV_USER_ALIAS}'@'%' identified by '${password}';
+			grant all privileges on *.* to '${user}'@'%';
+			SQL
 		fi
 
 		if [ -f $INIT_DIR/credentials ]
@@ -47,27 +62,13 @@ function init()
 					table="`$table`"
 				fi
 
-				# TABS must lead!!!
 				cat <<- SQL
 				create user if not exists '${user}'@'${host}' identified by '${password}';
-				grant all privileges on \`${space}\`.${table} to '${user}'@'$host';
+				grant all privileges on \`${space}\`.\`${table}\` to '${user}'@'${host}';
 				SQL
 			done < $INIT_DIR/credentials
 
 			echo 'flush privileges;'
-            echo
-		fi
-
-		if [ -d $INIT_DIR/schema ]
-		then
-			for schema in $(ls $INIT_DIR/schema/*.sql)
-			do
-				space=$(basename $schema .sql) # XXX Test not busybox
-				echo "-- $schema --"
-				echo "create database if not exists \`$space\`;"
-				echo "use \`$space\`;"
-				cat $schema
-			done
             echo
 		fi
 	} | tee $sql_tmp | mysql
